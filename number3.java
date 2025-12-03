2e88563b74b2b55109f3ecb27e3f2eee7288a0ad
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-03 19:41:56 +0800

    Add number3.java to print asterisks in a pattern

---
+++ number3.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class zai here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class number3
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {     
+            for (int j = 1; j <= i; j++) { 
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
