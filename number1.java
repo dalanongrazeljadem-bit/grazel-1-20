c1a72ec8fb1477fff19f7b721bd679ab0e8ffce4
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-03 18:50:17 +0800

    Create number1.java to validate user input
    Fix typo in output message for invalid number.

---
+++ number1.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class number here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class number1 
+{
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] number= new int [1];
+        int num;
+        
+        do{
+            System.out.print("Enter a number between 1 and 100: ");
+            num =in.nextInt( );
+            
+        } while ( num < 1 || num > 100);
+        
+        number[0] = num;
+        
+        System.out.println("Invlid number stored in array: " + number [0]);    
+        }
+    }
+    
+
