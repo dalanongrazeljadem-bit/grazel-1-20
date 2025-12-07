a4f64e7526a4dd10ff18a0c2dc695226a8b3ef4b
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 13:36:39 +0800

    Update number19.java

--- number19.java
+++ number19.java
@@ -9,13 +9,10 @@ import java.util.Scanner;
 public class no19{
     public static void main(String[] args) {
         
-        // Array of student names
+        
         String[] names = {"hannah", "grazel", "reshea", "jafet", "jefrrey"};
         
-        // Matching grades (same index as names)
         int[] grades = {85, 92, 76, 88, 95};
-        
-        // Display each student with matching grade
         System.out.println("Student Grades:");
         for (int i = 0; i < names.length; i++) {
             System.out.println(names[i] + " = " + grades[i]);
