15d7b826b822e6906dee31a9b14df58fba73aa41
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 13:10:08 +0800

    Change target check to even number check

--- number10.java
+++ number10.java
@@ -9,17 +9,17 @@ public class g10
 {
    public static void main(String[] args) {
         int[] numbers = {5, 14, 7, 12, 9};
-        int target = 14;
 
         int index = -1;  
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == target) {
+            if (numbers[i] % 2 == 0) {
                 index = i;
-                break;       
+                break;
+            
             }
         }
         System.out.println("Array: " + Arrays.toString(numbers));
-        System.out.println("First index of " + target + " is: " + index);
+        System.out.println("First index of " + " is: " + index);
     }
 }
