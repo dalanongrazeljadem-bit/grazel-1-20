93a6b3d7e95892fba0f735660a5ddd4777fc1699
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 13:26:20 +0800

    Rename class and update comments in number13.java

--- number13.java
+++ number13.java
@@ -1,26 +1,26 @@
-/**
- * Write a description of class no13 here.
+* Write a description of class seven here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class no13
+import java.util.*;
+public class g13
 {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
         int[] grades = {85, 72, 95, 69, 88, 67, 76, 59, 95};
         
         int passed = 0;
         int failed = 0;
 
-        // Loop through the array
+        
         for (int grade : grades) {
             if (grade > 75) {
                 passed++;
             } else if (grade < 75) {
                 failed++;
             }
-            // If grade == 75, it is ignored (neither pass nor fail)
+        
         }
 
         System.out.println("Number of students who passed (>75): " + passed);
