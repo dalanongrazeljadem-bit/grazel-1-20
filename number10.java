1c7db79553bcb458d5c868045dbac4ef8996f867
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 13:01:31 +0800

    Update number10.java

--- number10.java
+++ number10.java
@@ -1,24 +1,25 @@
 /**
- * Write a description of class number10 here.
+ * Write a description of class seven here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class number10
+import java.util.*;
+public class g10
 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         int[] numbers = {5, 14, 7, 12, 9};
         int target = 14;
 
-        int index = -1;  // stores result
+        int index = -1;  
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == target) {
-                index = i;  // store first index
-                break;       // stop early
+                index = i;
+                break;       
             }
         }
-
+        System.out.println("Array: " + Arrays.toString(numbers));
         System.out.println("First index of " + target + " is: " + index);
     }
 }
