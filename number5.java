6a36e99fed09c33e47ebca3ecff9cf341420f811
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 12:21:55 +0800

    Update target value and numbers array in main method

--- number5.java
+++ number5.java
@@ -4,18 +4,21 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
 public class number5
 {
     public static void main(String[] args) {
 
-        int[] numbers = {10, 25, 37, 42, 55, 67};
-        int target = 42; // the value we want to find
+        int[] numbers = {10, 25, 22, 45, 55, 67};
+        int target = 22; 
+        System.out.println("Searching for " + target + " in " + Arrays. toString(numbers));
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == target) {
                 System.out.println("Value " + target + " found at index " + i);
-                break; // stop the loop immediately
+                break; 
             }
         }
     }
 }
+
