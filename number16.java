60c1f22f22c5105e7dbc29f6cadc05579a3cbb7e
dalanongrazeljadem-bit <dalanongrazeljadem@gmail.com>
2025-12-07 10:40:17 +0800

    Add number16.java to count digits in input

---
+++ number16.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class xrey here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class no16
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter number: ");
+        
+        String num= scanner.nextLine();
+        int digitCount = num.length();
+        
+        System.out.println("Digits: " + digitCount);
+        scanner.close();
+        
+        
+        
+        
+    }
+}
